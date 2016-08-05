c9c99267914ee3848087085e17e1cba5c3154ebc
thefreshduke <scottyshaw1@gmail.com>
2016-08-05 11:55:24 -0700

    Connected IBOutlets and IBActions

--- PavelStopwatch/ViewController.swift
+++ PavelStopwatch/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBAction func startTime(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func stopTime(sender: AnyObject) {
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +31,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
