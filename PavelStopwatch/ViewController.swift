a1129bde61ebd0a622ce2cf91241b043e37d3e26
thefreshduke <scottyshaw1@gmail.com>
2016-08-05 12:09:20 -0700

    Completed basic stopwatch

--- PavelStopwatch/ViewController.swift
+++ PavelStopwatch/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    var counter = 0
 
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var stopButton: UIButton!
     
     @IBAction func startTime(sender: AnyObject) {
-        
+        validateTimer()
     }
     
     @IBAction func stopTime(sender: AnyObject) {
-        
+        timer.invalidate()
+        counter = 0
     }
     
     override func viewDidLoad() {
@@ -30,6 +34,16 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func validateTimer() {
+        let repeatingFunction = #selector(ViewController.updateTime)
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: repeatingFunction, userInfo: nil, repeats: true)
+    }
+    
+    func updateTime() {
+        counter += 1
+        timeLabel.text = "\(counter)"
     }
 }
 
